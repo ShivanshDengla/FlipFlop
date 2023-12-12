f20d4776c570fea02e973cb4e7b78cabb5fc2206
Tickether <tickether@gmail.com>
2023-12-12 13:18:58 +0000

    merge old FlipFlop

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,37 @@ import SwapModal from "../components/SwapModal";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Index() {
   return (
-    <div className="px-8 bg-gray-100 min-h-screen relative">
+    <>
+    <Head>
+      <title>FlipFlop</title>
+      <meta
+        content="Front End to Access the PoolTogether V5 smart contracts"
+        name="with the help of decent.xys's The Box Hooks, we can swap tokens directly from V4 to V5 PoolTogether"
+      />
+      <link href="" rel="icon" />
+    </Head>
+    <main className="px-8 bg-gray-100 min-h-screen relative">
       <div className="flex justify-between py-4 items-center">
+        <div>
+          <Image 
+            src='/logo.png'  
+            alt="Logo" 
+            height={20}
+            width={20}
+            priority={true} 
+          />
+          <p>FlipFlop</p>
+        </div>
         <a
           href="https://checkout.decent.xyz/"
           target="_blank"
           className="py-2 px-4 bg-white rounded-md drop-shadow-md font-medium flex items-center gap-2"
         >
+          
           <div>
             <Image
               src="/decent-icon-black.png"
@@ -38,6 +59,8 @@ export default function Index() {
           Decent
         </Link>
       </div>
-    </div>
+    </main>
+    </>
+    
   );
 }
